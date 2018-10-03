e3d74e5ba719e183793cb7673939b224c82c7ec4
Szero <singleton@tfwno.gf>
2018-10-03 04:28:51 +0200

    remove excess stuff from setup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
-
 """Setting it all up like a real human bean"""
 
 import re
-
 from setuptools import setup
+
 
 def version(filename):
     """Thanks python!"""
     with open(filename) as filep:
         return re.search('__version__ = "(.+?)"', filep.read()).group(1)
+
 
 setup(
     name="stuff2str",
@@ -20,7 +20,6 @@ setup(
     license="MIT",
     author="Szero",
     author_email="singleton@tfwno.gf",
-    packages=['stuff2str'],
     entry_points={"console_scripts": ["stuff2str = stuff2str.__main__:run"]},
     classifiers=[
         "Environment :: Console",
@@ -28,12 +27,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3",
         "Topic :: Utilities :: Build_Systems",
         "Topic :: JavaScript"
     ],
-    install_requires=["path.py>=10.0"]
-    )
+    install_requires=["path.py>=10.0"])
